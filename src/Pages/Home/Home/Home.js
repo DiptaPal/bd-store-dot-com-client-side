3b8ebea02d4283774c0f535f8954c7208283501b
DiptaPal <diptapal1998@gmail.com>
2022-11-23 21:32:42 +0600

    initial setup done

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useTitle from '../../../Hooks/useTitle';
+
+const Home = () => {
+    useTitle('Home')
+    return (
+        <div>
+            <h2>This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
