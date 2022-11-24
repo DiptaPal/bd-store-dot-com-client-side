aef8f70fd9d283c62ef59d4138bc9fe032d0a217
DiptaPal <diptapal1998@gmail.com>
2022-11-24 23:50:57 +0600

    seller and buyer users get successfully

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const AllBuyers = () => {
+    const { data: buyers = [], refetch } = useQuery({
+        queryKey: ['buyers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/buyers')
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
         <div>
-            
+            <div className='px-10'>
+                <h3 className='text-3xl m-7'>My Buyers</h3>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Buyer Profile</th>
+                                <th>Buyer Name</th>
+                                <th>Email</th>
+                                <th>Verification</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {buyers &&
+                                buyers?.map((buyer, i) => <tr
+                                    key={buyer._id}
+                                    className={`cursor-pointer ${i % 2 === 1 ? 'hover' : ''}`}
+                                >
+                                    <th>{i + 1}</th>
+                                    <td><img src={buyer.img} className='w-16 h-16 rounded-full' alt="" /></td>
+                                    <td>{buyer.name}</td>
+                                    <td>{buyer.email}</td>
+                                    <td><button className='btn btn-success text-white'>Verify</button></td>
+                                    <td><button className='btn btn-error text-white'>Delete</button></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const AllSellers = () => {
+    const { data: sellers=[], refetch } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/sellers')
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    const handleDeleted = () =>{
+
+    }
+
     return (
         <div>
-            <h2>This is All Seler Page</h2>
+            <div className='px-10'>
+                <h3 className='text-3xl m-7'>My Buyers</h3>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Seller Profile</th>
+                                <th>Seller Name</th>
+                                <th>Email</th>
+                                <th>Verification</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sellers &&
+                                sellers?.map((seller, i) => <tr
+                                    key={seller._id}
+                                    className={`cursor-pointer ${i % 2 === 1 ? 'hover' : ''}`}
+                                >
+                                    <th>{i + 1}</th>
+                                    <td><img src={seller.img} className='w-16 h-16 rounded-full' alt="" /></td>
+                                    <td>{seller.name}</td>
+                                    <td>{seller.email}</td>
+                                    <td><button className='btn btn-success text-white'>Verify</button></td>
+                                    <td><button onClick={() =>handleDeleted(seller._id)} className='btn btn-error text-white'>Delete</button></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
